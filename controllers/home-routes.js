d3f50cc6ace015486224b15c96926a0155c492dc
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-03-26 14:17:50 -0400

    fixed home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,23 +3,23 @@ const Blog = require('../models/Blog');
 
 // route for all blogs
 router.get('/', async (req, res) => {
-    const blogData = await Blog.findAll().catch((err) => {
-        res.json(err);
-    });
-    const blogs = blogData.map((blog) => blog.get({ plain: true }));
-    res.render('all', { blogs });
+    try {
+        const blogData = await Blog.findAll();
+        res.status(200).json(blogData);        
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 // route for one blog
-router.get('./blog/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
     try {
         const blogData = await Blog.findByPk((req.params.id));
         if(!blogData) {
             res.status(404).json({message: 'No blog with this id!'});
             return;
         }
-        const blog = blogData.get({ plain: true });
-        res.render('blog', blog)
+        res.status(200).json(blogData);
     } catch (err) {
         res.status(500).json(err);
     }
